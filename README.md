6ef1c0f3037df115e08393e610b2cf8824aaace7
Maicon-g14 <maicon_gabriel12@hotmail.com>
2022-06-16 01:15:05 -0300

    notificador do chat de voz funcional

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 A Discord bot that notifies when someone enters a voice chat sending a message on its specific channel. 
 Specifies whose voice chat the person has entered, who entered is optional.
 
-Start command: `ts-node index.ts`
+Start command: `npx ts-node index.ts`
--- index.ts
+++ index.ts
@@ -1,21 +1,28 @@
-import DiscordJS, { Intents } from 'discord.js';
+import DiscordJS, { Intents, TextChannel } from 'discord.js';
 import dotenv from 'dotenv';
 
 dotenv.config();
+
+// let pessoasNoChatDeVoz = 0
 
 const client = new DiscordJS.Client({
     intents: [
         Intents.FLAGS.GUILDS,
         Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_VOICE_STATES
     ],
 });
 
 client.on('ready', () => {
     const message = 'Ohayooo! Asa dayo! \\OwO/';
     console.log(message);
+
+    const channel = (client.channels.cache.get('912493062712872983') as TextChannel);
+    channel.send('A WILD KOMACHI HAS RISEN!');
+
 });
 
-client.on('messageCreate', (message) => {
+client.on('messageCreate', async (message) => {
     const command = message.content.split(' ');
     let response = '';
 
@@ -35,6 +42,38 @@ client.on('messageCreate', (message) => {
             message.reply({ content: response });
         }
     }
+
+    
+    //let voiceChatMembers = message.guild.channels.get('912493062712872984').members;
+
+    // if(voiceChatMembers && voiceChatMembers.size > pessoasNoChatDeVoz) {
+    //     pessoasNoChatDeVoz++
+    //     const channel = (client.channels.cache.get('912493062712872983') as TextChannel);
+    //     channel.send('Alguem entrou no chat de voz ' + pessoasNoChatDeVoz);
+
+    // } else if(voiceChatMembers && voiceChatMembers.size < pessoasNoChatDeVoz) {
+    //     pessoasNoChatDeVoz--
+    //     const channel = (client.channels.cache.get('912493062712872983') as TextChannel);
+    //     channel.send('Alguem saiu do chat de voz ' + pessoasNoChatDeVoz);
+    // }
 });
 
+
+client.on('voiceStateUpdate', (oldMember, newMember) => {
+    let newUserChannel = newMember.channel
+    let oldUserChannel = oldMember.channel
+    const channel = (client.channels.cache.get('912493062712872983') as TextChannel);       //id do chat de audio
+
+
+    if(!oldUserChannel && newUserChannel) {
+        channel.send('Alguem entrou no chat de voz ');
+
+    } else if(!newUserChannel){
+        channel.send('Alguem saiu do chat de voz ');
+
+    }
+})
+
+
 client.login(process.env.TOKEN);
+
